4cbc70c1597d5f9837e55ee9ab4e2dd4929ddf1c
RODRIGO NEGRON <rodrigoo.n@hotmail.com.argit config --global user.email rodrigoo.n@hotmail.com.ar>
2021-05-21 17:42:02 -0300

    deteled extra services

--- src/services/posts.js
+++ src/services/posts.js
@@ -1,15 +1,5 @@
 import axios from "axios";
 
-export const getAllPosts = async () => {
-  const request = axios.get("https://jsonplaceholder.typicode.com/posts");
-  return request.then((response) => response.data);
-};
-
-export const detail = (id) => {
-  const request = axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
-  return request.then((response) => response.data);
-};
-
 export const createPost = (newObject) => {
   const request = axios.post(
     "https://jsonplaceholder.typicode.com/posts",
